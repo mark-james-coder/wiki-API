7e26b6d24b0ae7fbe12fcd39f37e8b15dc9c0aa8
Mark <markjpoole@yahoo.com>
2021-03-15 18:16:08 +0000

    set up project, connection to database

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = {
+ title: String,
+ content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
