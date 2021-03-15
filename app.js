738cc1ec9c183cee18d80a5ad334430cbab84ec1
Mark <markjpoole@yahoo.com>
2021-03-15 21:00:25 +0000

    refactor code by chaining route handlers

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res) {
+app.route("/articles")
+
+.get(function(req, res) {
   Article.find(function(err, foundArticles){
     if(!err) {
       res.send(foundArticles);
@@ -29,9 +31,9 @@ app.get("/articles", function(req, res) {
       res.send(err);
     }
   });
-});
+})
 
-app.post("/articles", function(req, res) {
+.post(function(req, res) {
 
   const newArticle = new Article({
     title: req.body.title,
@@ -44,9 +46,9 @@ app.post("/articles", function(req, res) {
       res.send(err);
     }
   });
-});
+})
 
-app.delete("/articles", function(req, res) {
+.delete(function(req, res) {
   Article.deleteMany(function(err){
     if (!err) {
       res.send("Successfully deleted all articles.");
@@ -55,6 +57,8 @@ app.delete("/articles", function(req, res) {
     }
   });
 });
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
