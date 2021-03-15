51773da55b4684537228f030ef663b887b1ed260
Mark <markjpoole@yahoo.com>
2021-03-15 20:23:52 +0000

    get all articles

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const articleSchema = {
@@ -12,14 +21,16 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-const app = express();
+app.get("/articles", function(req, res) {
+  Article.find(function(err, foundArticles){
+    if(!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static("public"));
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
