4de6e47f96f66d49bf4a62c7bfa72c77d9e9203d
Mark <markjpoole@yahoo.com>
2021-03-16 17:02:41 +0000

    DELETE a specific article

--- app.js
+++ app.js
@@ -113,9 +113,23 @@ app.route("/articles/:articleTitle")
         }
       }
     );
+  })
+
+  .delete(function(req, res) {
+
+    Article.deleteOne({
+        title: req.params.articleTitle
+      }
+
+      function(err) {
+        if (!err) {
+          res.send("Successfully deleted article.")
+        } else {
+          res.send(err);
+        }
+      }
+    );
   });
-
-
 
 
 
